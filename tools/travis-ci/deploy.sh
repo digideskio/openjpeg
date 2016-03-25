f0c0b9172a7d8e4f01ea781f78515f37682d9dee
Julien Malik <julien.malik@paraiso.me>
2016-03-25 17:18:58 +0100

    Add set -x to deploy script

--- tools/travis-ci/deploy.sh
+++ tools/travis-ci/deploy.sh
@@ -2,8 +2,10 @@
 
 # This script executes the 'deploy' step when running under travis-ci
 
+set -x
+
 openssl aes-256-cbc -K $encrypted_02a35d19a032_key -iv $encrypted_02a35d19a032_iv -in tools/travis-ci/id_rsa_filetransfer.enc -out ~/.ssh/id_rsa -d
-eval "$(ssh-agent -s)" #start the ssh agent
+eval "$(ssh-agent -s)"
 chmod 600 ~/.ssh/id_rsa
 ssh-add ~/.ssh/id_rsa
 scp build/openjpeg-*.zip ${DEPLOY_TARGET}
