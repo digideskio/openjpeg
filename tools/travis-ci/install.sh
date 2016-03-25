250bd5aa61cb3d0dd25a538198bfbc4f7e11687e
Julien Malik <julien.malik@paraiso.me>
2016-03-25 11:57:18 +0100

    Always install latest cmake version

--- tools/travis-ci/install.sh
+++ tools/travis-ci/install.sh
@@ -35,12 +35,10 @@ if [ "${COVERITY_SCAN_BRANCH:-}" == "1" ] || [ "${OPJ_CI_ABI_CHECK:-}" == "1" ];
 	exit 0
 fi
 
-if [ "${OPJ_CI_ASAN:-}" == "1" ]; then
-	# We need a new version of cmake than travis-ci provides
-	wget --no-check-certificate -qO - https://cmake.org/files/v3.3/cmake-3.3.2-Linux-x86_64.tar.gz | tar -xz
-	# copy to a directory that will not changed every version
-	mv cmake-3.3.2-Linux-x86_64 cmake-install
-fi
+# We need a new version of cmake than travis-ci provides
+wget --no-check-certificate -qO - https://cmake.org/files/v3.3/cmake-3.3.2-Linux-x86_64.tar.gz | tar -xz
+# copy to a directory that will not changed every version
+mv cmake-3.3.2-Linux-x86_64 cmake-install
 
 if [ "${OPJ_CI_SKIP_TESTS:-}" != "1" ]; then
 
