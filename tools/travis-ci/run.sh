51d8f259ad150bfe68ad24ca9c7f7d21abe0dc08
Julien Malik <julien.malik@paraiso.me>
2016-03-29 19:23:04 +0200

    Forward env. var.

--- tools/travis-ci/run.sh
+++ tools/travis-ci/run.sh
@@ -7,10 +7,24 @@ set -o nounset   ## set -u : exit the script if you try to use an uninitialised 
 set -o errexit   ## set -e : exit the script if any statement returns a non-true return value
 set -o pipefail  ## Fail on error in pipe
 
-if [ "${OPJ_CI_DOCKER:-}" == "true" ] && [ "${OPJ_CI_INSIDE_DOCKER:-}" != "true" ]; then
+if [ "${OPJ_CI_DOCKER:-}" == "true" ]; then
+    echo "Building inside docker container..."
+
     # run this script inside the docker container
-    # mount home from the host to the same place on the container
-    docker run --rm=true -e OPJ_CI_INSIDE_DOCKER=true -v $HOME:$HOME:rw jmk/centosbuilder /bin/bash -c "$TRAVIS_BUILD_DIR/tools/travis-ci/run.sh"
+    # - mount home from the host to the same place on the container
+    # - forward env. var. from this script to the docker daemon (except OPJ_CI_DOCKER !)
+
+    # Forward OPJ_ specific vars
+    for envvar in OPJ_CI_ARCH OPJ_CI_BUILD_CONFIGURATION OPJ_CI_BUILD_SHARED_LIBS OPJ_CI_CPACK_SYSTEM_NAME ; do
+        echo $envar=${!envar} >> dockerenv
+    done
+
+    # Forward TRAVIS env. var.
+    for envvar in TRAVIS_BRANCH TRAVIS_BUILD_DIR TRAVIS_BUILD_ID TRAVIS_BUILD_NUMBER TRAVIS_COMMIT TRAVIS_COMMIT_RANGE TRAVIS_JOB_ID TRAVIS_JOB_NUMBER TRAVIS_OS_NAME TRAVIS_PULL_REQUEST TRAVIS_REPO_SLUG TRAVIS_SECURE_ENV_VARS TRAVIS_TAG ; do
+        echo $envar=${!envar} >> dockerenv
+    done
+
+    docker run --rm=true --env-file=dockerenv -v $HOME:$HOME:rw jmk/centosbuilder /bin/bash -c "cd $TRAVIS_BUILD_DIR && ./tools/travis-ci/run.sh"
     exit $?
 fi
 
