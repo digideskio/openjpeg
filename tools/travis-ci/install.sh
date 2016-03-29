7fa34cb0e1a8e26c8520ca38feb3d1fe22f1b1a8
Julien Malik <julien.malik@paraiso.me>
2016-03-29 23:11:36 +0200

    Install cmake in docker container

--- tools/travis-ci/install.sh
+++ tools/travis-ci/install.sh
@@ -35,7 +35,7 @@ if [ "${COVERITY_SCAN_BRANCH:-}" == "1" ] || [ "${OPJ_CI_ABI_CHECK:-}" == "1" ];
 	exit 0
 fi
 
-if [ "${OPJ_CI_ASAN:-}" == "1" ]; then
+if [ "${OPJ_CI_ASAN:-}" == "1" ] || [ "${OPJ_CI_DOCKER:-}" == "true" ]; then
 	# We need a new version of cmake than travis-ci provides
 	wget --no-check-certificate -qO - https://cmake.org/files/v3.3/cmake-3.3.2-Linux-x86_64.tar.gz | tar -xz
 	# copy to a directory that will not changed every version
